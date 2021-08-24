568907647e753753cc83aba67c0940079259b4f7
davy1ex <32157455+davy1ex@users.noreply.github.com>
2021-08-24 19:34:41 +0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,32 +1,33 @@
-/*
- * main.c
- *
- * Created: 24-Jun-21 12:46:01 AM
- *  Author: dezy1ex
- */ 
-
-#include <xc.h>
-#include <avr/io.h>
-
-#define leftSen PC2                //Connect Left Sensor At PA0
-#define rightSen PC3               //Connect Right Sensor At PA1
+#include<avr/io.h>
+#define leftSen PC0					//Connect Left Sensor At PC0
+#define rightSen PC1				//Connect Right Sensor At PC1
 
 int main(void)
 {
-	DDRC=0b0000000;                // make PA0,PA1 as input for both sensors 0x0b11111100
-	DDRB=0b00011110;				//DEFINE PB1,PB2,PB3,PB4 AS OUTPUT LINES 
-	int sensor=0;
+	DDRC=0b00000000;						// make PC0,PC1 as input for both sensors 0b11111100
+	DDRB=0b00000011;						// make Port  as output to connect motor pins 
 	
 	while(1)
 	{
-		sensor = PINC & 0b0000011; //STATEMENT TO STORE VALUE FROM PIN REGISTER TO VARIABLE
-		if(sensor==0b0000011)     //CONDITIONAL STATEMENTS
-		{PORTB=0b00001010;}
-		else if (sensor==0b0000001)
-		{PORTB=0b00000010;}
-		else if(sensor==0b0000010)
-		{PORTB=0b00001000;}
-		else                       //CONDITION WHEN BOTH SENSORS ARE OFF
-		{PORTB=0b00000000;}
+		PINC=0x03;					//initialize PC0 and PC1 0b00000011
+		if(bit_is_clear(PINC,leftSen)){        // check if left sensor is OFF
+			
+			if(bit_is_clear(PINC,rightSen)) {    // check if right sensor is OFF
+				PORTB=0b00000000;       // if both sensor zero
+			}       // then stop the robot
+			else{
+				PORTB=0b00000001;                  // if right is ON then take left
+			}
+		}
+		
+		else                                  // check if left sensor in ON
+		{
+			if(bit_is_clear(PINC,rightSen)) {   // check if right sensor is OFF
+				PORTB=0b00000010;      // it means left sensor is ON
+			}      // so take right
+			else{
+				PORTB=0b00000011;            // if both sensor is ON
+			}     // then keep moving the robot
+		}
 	}
 }
